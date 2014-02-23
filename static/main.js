ec13a14b82486bcc038d117a25b337194dffc997
Casey Falk <caseyfalk94@gmail.com>
2014-02-23 01:00:29 -0500

    Added members page, utilized jparallax, and updated members list.

---
+++ static/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+$('#currentMembers .memberList').parallax({
+	mouseport: $(".viewport#currentMembers"),
+	xparallax:false
+});
+
+$('#emeritusMembers .memberList').parallax({
+	mouseport: $(".viewport#emeritusMembers"),
+	xparallax:false
+});
+
+
+});
